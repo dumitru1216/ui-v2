8cf3988fc9d85ffd124d2764229def03dc2600cd
Dutu1337 <46423190+dumitru1216@users.noreply.github.com>
2023-07-22 06:03:46 -0700

    sdk

---
+++ ui v2/c_includes.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <ostream> 
+#include <vector>
---
+++ ui v2/sdk/c_sdk.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include "../c_includes.hpp"
+
+namespace sdk {
+	using c_str = std::string;
+	using c_str_vec = std::vector<std::string>;
+	using c_funcion = void;
+
+	/* math */
+	namespace math {
+		struct vec2_t {
+			/* default builders */
+			vec2_t( ) : x( 0 ), y( 0 ) { }
+			vec2_t( float x, float y ) : x( x ), y( y ) { }
+			~vec2_t( ) { }
+
+			/* parameters */
+			float x;
+			float y;
+		};
+	}
+
+	/* color */
+	namespace color {
+		struct col_t {
+			col_t( ) : r( 255 ), g( 255 ), b( 255 ), a( 255 ) { } /* default white */
+			col_t( int r, int g, int b, int a = 255 ) : r( r ), g( g ), b( b ), a( a ) { }
+			~col_t( ) { }
+
+			std::uint32_t g_convert( ) {
+				std::uint32_t out = 0;
+
+				out = static_cast< std::uint32_t >( this->r ) << 0;
+				out |= static_cast< std::uint32_t >( this->g ) << 8;
+				out |= static_cast< std::uint32_t >( this->b ) << 16;
+				out |= static_cast< std::uint32_t >( this->a ) << 24;
+
+				return out;
+			}
+
+			col_t g_modify_alpha( int a ) {
+				return col_t( r, g, b, a );
+			}
+
+			col_t g_darker( int amount ) {
+				int red = r;
+				int green = g;
+				int blue = b;
+
+				// reduce the RGB values by the given amount
+				red = std::fmax( 0, red - amount );
+				green = std::fmax( 0, green - amount );
+				blue = std::fmax( 0, blue - amount );
+
+				return col_t( red, green, blue, a );
+			}
+
+			int r, g, b, a;
+		};
+	}
+}
