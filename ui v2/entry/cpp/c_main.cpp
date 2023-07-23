d648834193687b134d8d7af8a0fd566e35bdfdf8
Dutu1337 <46423190+dumitru1216@users.noreply.github.com>
2023-07-23 02:08:04 -0700

    Update c_main.cpp

--- ui v2/entry/cpp/c_main.cpp
+++ ui v2/entry/cpp/c_main.cpp
@@ -45,7 +45,44 @@ int install( sdk::c_instance instance, sdk::c_instance prev_instance,
 			continue;
 		}
 
+		sdk::c_warper->warp_frame( );
+		sdk::c_warper->begin_frame( ); {
+			/* run there */
+		}
+		sdk::c_warper->end_frame( );
 
+		/* render states and clear device */
+		sdk::c_warper->set_render_state( i::device, D3DRS_ZENABLE, false );
+		sdk::c_warper->set_render_state( i::device, D3DRS_ALPHABLENDENABLE, false );
+		sdk::c_warper->set_render_state( i::device, D3DRS_SCISSORTESTENABLE, false );
+
+		/* clear */
+		sdk::c_warper->clear(
+			i::device, 0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_RGBA( 23, 23, 23, 0 ), 1.0f, 0
+		);
+
+		/* init scene */
+		if ( sdk::c_warper->begin_scene( i::device ) >= 0 ) {
+			i::c_hooks->init_device( i::device, [ = ]( ) {
+				/* here we are going to render our stuff */
+				ImGui::Render( );
+				ImGui_ImplDX9_RenderDrawData( ImGui::GetDrawData( ) );
+			} );
+
+
+			/* unload external window */
+			if ( GetAsyncKeyState( VK_END ) ) {
+				ExitProcess( -1 );
+			}
+
+			/* end scene */
+			sdk::c_warper->end_scene( i::device );
+		}
+
+		/* handle packet loss - i will handle it tmr */
+		HRESULT result{ i::device->Present( NULL, NULL, NULL, NULL ) };
+		if ( result == D3DERR_DEVICELOST && i::device->TestCooperativeLevel( ) == D3DERR_DEVICENOTRESET )
+			i::c_hooks->reset_device( );
 	}
 }
 
